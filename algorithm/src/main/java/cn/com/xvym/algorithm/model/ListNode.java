22ee6aa8a8667bb3a896f553e9f0d1f867b37731
xvym <xdxvym@163.com>
2021-12-19 20:38:15 +0800

    add: init dependency

--- algorithm/src/main/java/cn/com/xvym/algorithm/model/ListNode.java
+++ algorithm/src/main/java/cn/com/xvym/algorithm/model/ListNode.java
@@ -19,4 +19,40 @@ public class ListNode {
         this.next = next;
     }
 
+    public ListNode BuildDefaultListNode(int count) {
+        ListNode head = new ListNode();
+        ListNode cur = new ListNode();
+        ListNode next = new ListNode();
+        for (int i = 0; i <= count; i++) {
+            cur.val = i;
+            if (i == 1) {
+                head = cur;
+            }
+            if (i != count) {
+                cur.next = next;
+                cur = cur.next;
+                next = new ListNode();
+            }
+        }
+        return head;
+    }
+
+    public ListNode BuildListNode(int[] nums) {
+        ListNode head = new ListNode();
+        ListNode cur = new ListNode();
+        ListNode next = new ListNode();
+        for (int i = 0; i < nums.length; i++) {
+            cur.val = i;
+            if (i == 0) {
+                head = cur;
+            }
+            if (i != nums.length - 1) {
+                cur.next = next;
+                cur = cur.next;
+                next = new ListNode();
+            }
+        }
+        return head;
+    }
+
 }
